453888961847e6099882ef96adbd2c542b66b397
shwibi <shwibishrabs285@gmail.com>
2021-08-06 22:37:54 +0530

    Auto ticket fetching

--- commands/Developer Portal/todo.js
+++ commands/Developer Portal/todo.js
@@ -4,19 +4,12 @@ const Index = require(`../../index`);
 const fs = require(`fs`);
 const mongoose = require(`mongoose`);
 const Message = require(`../../events/message`);
-const {
-  Cache,
-  Err,
-  Main
-} = require(`../../utils/Utils`);
-const {
-  remove: replace
-} = require("../../models/Guilds");
+const { Cache, Err, Main } = require(`../../utils/Utils`);
+const { remove: replace } = require("../../models/Guilds");
 
-const CommandName = 'ToDo';
+const CommandName = "ToDo";
 
 class Command extends Message.Event {
-
   /**
    * Create command
    */
@@ -25,8 +18,8 @@ class Command extends Message.Event {
   }
 
   /**
-   * 
-   * @param {Discord.Client} client 
+   *
+   * @param {Discord.Client} client
    */
   init(client) {
     if (this.initiated) return;
@@ -36,20 +29,33 @@ class Command extends Message.Event {
 
   /**
    * Call this command using message
-   * @param {Discord.Message} message 
+   * @param {Discord.Message} message
    */
   async call(message) {
-
-    if (!this.initiated) return new Err(`Called ${CommandName} command tendril without message initiation!`);
+    if (!this.initiated)
+      return new Err(
+        `Called ${CommandName} command tendril without message initiation!`
+      );
 
     const args = message.content.split(/\s/);
     const commandRaw = args.shift();
-    const ticketID = args.shift();
+    const raw = args.shift();
 
-    const channel = this.client.channels.cache.find(c => c.id == this.config.Dev.todo_channel);
+    const channel = this.client.channels.cache.find(
+      (c) => c.id == this.config.Dev.todo_channel
+    );
+
+    let ticketID = 0;
+    channel.messages.fetch({ limit: 10 }).then((collected) => {
+      collected.forEach((m) => {
+        const ticket = m.content.split(/\s/).shift();
+        const fetchTicketId = ticket.split(this.e.tic).join("").trim();
+        const parseId = parseInt(fetchTicketId);
+        if (parseId > ticketID) ticketID = fetchTicketId;
+      });
+    });
 
     if (["done", "off", "working", "delete"].includes(ticketID.toLowerCase())) {
-
       let replaceBy = this.e.on;
 
       switch (ticketID.toLowerCase()) {
@@ -70,119 +76,140 @@ class Command extends Message.Event {
           break;
       }
 
-
       this.channelFetch(channel, args, replaceBy, (err) => {
         if (err) return this.InLog(err);
         message.channel.send(`${this.e.r} Marked ${args[0]} as ${ticketID}!`);
-      })
-
-
+      });
 
       return;
-
     }
 
     const todoContent = args.join(" ");
-    if (!todoContent || !ticketID) return message.channel.send(`${this.e.x} Please send a todo message!`);
+    if (!todoContent || !ticketID)
+      return message.channel.send(`${this.e.x} Please send a todo message!`);
 
     const toSend = `${this.e.tic}${ticketID} ***[${message.author.username}]*** - ${todoContent} ${this.e.off}`;
     if (channel) {
-      channel.send(toSend).then(
-
-        async (msgSent) => {
-          const thread = await channel.threads?.create({
-            name: ticketID
-          });
-          if(thread) {
-            await thread.members?.add(message.author.id);
-            await thread.send(msgSent.content);
-          }
-          this.delete(message);
-          message.channel.send(`${this.e.r} Added todo with **ID ${ticketID}**!`).then(msg => this.delete(msg, 5));
+      channel.send(toSend).then(async (msgSent) => {
+        const thread = await channel.threads?.create({
+          name: ticketID,
+        });
+        if (thread) {
+          await thread.members?.add(message.author.id);
+          await thread.send(msgSent.content);
         }
-
-      )
+        this.delete(message);
+        message.channel
+          .send(`${this.e.r} Added todo with **ID ${ticketID}**!`)
+          .then((msg) => this.delete(msg, 5));
+      });
     }
-
   }
 
-  async channelFetch(channel = new Discord.Channel(), args = [], replace = this.e.on, callback = () => {}) {
-    channel.messages.fetch({
-      limit: 10
-    }).then(async msgs => {
-      // this.InLog(msgs);
-      msgs.forEach(async msg => {
-        if (msg.partial) {
-          msg.fetch().then(
-            async fullMsg => {
-              if (fullMsg.author.id == this.client.user.id && fullMsg.content.includes(`${this.e.tic}${args[0]}`)) {
-                if (replace == "delete") {
-                  this.delete(msg);
-                  const ticketId = args[0];
-                  const thread = await channel.threads?.cache.find(th => th.name.includes(ticketId));
-                  if(thread) {
-                    await thread.delete();
-                  }
-                }
-                if(replace == this.e.on) {
-                  const ticketId = args[0];
-                  const thread = await channel.threads?.cache.find(th => th.name.includes(ticketId));
-                  if(thread) {
-                    await thread.setArchived(true);
-                  }
-                }
-                if(replace == this.e.off || replace == this.e.idle) {
-                  const ticketId = args[0];
-                  const thread = await channel.threads?.cache.find(th => th.name.includes(ticketId));
-                  if(thread) {
-                    await thread.setArchived(false);
-                  }
-                }
-                fullMsg.edit(this.replace(msg, replace));
-                return;
-              }
-            }
-          ).catch(err => {
-            new Err(err, err.code || "FETCHERR", {
-              err,
-              msg,
-              msgs
-            });
-          })
-        } else {
-          if (msg.author.id == this.client.user.id && msg.content.includes(`${this.e.tic}${args[0]}`)) {
-            if (replace == "delete") {
-              this.delete(msg);
-              const ticketId = args[0];
-              const thread = await channel.threads?.cache.find(th => th.name.includes(ticketId));
-              if(thread) {
-                await thread.delete();
-              }
-            }
-            if(replace == this.e.on) {
-              const ticketId = args[0];
-              const thread = await channel.threads?.cache.find(th => th.name.includes(ticketId));
-              if(thread) {
-                await thread.setArchived(true);
-              }
-            }
-            if(replace == this.e.off || replace == this.e.idle) {
-              const ticketId = args[0];
-              const thread = await channel.threads?.cache.find(th => th.name.includes(ticketId));
-              if(thread) {
-                await thread.setArchived(false);
-              }
-            }
-            msg.edit(this.replace(msg, replace));
-            return;
-          }
-        }
+  async channelFetch(
+    channel = new Discord.Channel(),
+    args = [],
+    replace = this.e.on,
+    callback = () => {}
+  ) {
+    channel.messages
+      .fetch({
+        limit: 10,
       })
-    }).then(callback(null)).catch(err => {
-      if (err) callback(err)
-    });
+      .then(async (msgs) => {
+        // this.InLog(msgs);
+        msgs.forEach(async (msg) => {
+          if (msg.partial) {
+            msg
+              .fetch()
+              .then(async (fullMsg) => {
+                if (
+                  fullMsg.author.id == this.client.user.id &&
+                  fullMsg.content.includes(`${this.e.tic}${args[0]}`)
+                ) {
+                  if (replace == "delete") {
+                    this.delete(msg);
+                    const ticketId = args[0];
+                    const thread = await channel.threads?.cache.find((th) =>
+                      th.name.includes(ticketId)
+                    );
+                    if (thread) {
+                      await thread.delete();
+                    }
+                  }
+                  if (replace == this.e.on) {
+                    const ticketId = args[0];
+                    const thread = await channel.threads?.cache.find((th) =>
+                      th.name.includes(ticketId)
+                    );
+                    if (thread) {
+                      await thread.setArchived(true);
+                    }
+                  }
+                  if (replace == this.e.off || replace == this.e.idle) {
+                    const ticketId = args[0];
+                    const thread = await channel.threads?.cache.find((th) =>
+                      th.name.includes(ticketId)
+                    );
+                    if (thread) {
+                      await thread.setArchived(false);
+                    }
+                  }
+                  fullMsg.edit(this.replace(msg, replace));
+                  return;
+                }
+              })
+              .catch((err) => {
+                new Err(err, err.code || "FETCHERR", {
+                  err,
+                  msg,
+                  msgs,
+                });
+              });
+          } else {
+            if (
+              msg.author.id == this.client.user.id &&
+              msg.content.includes(`${this.e.tic}${args[0]}`)
+            ) {
+              if (replace == "delete") {
+                this.delete(msg);
+                const ticketId = args[0];
+                const thread = await channel.threads?.cache.find((th) =>
+                  th.name.includes(ticketId)
+                );
+                if (thread) {
+                  await thread.delete();
+                }
+              }
+              if (replace == this.e.on) {
+                const ticketId = args[0];
+                const thread = await channel.threads?.cache.find((th) =>
+                  th.name.includes(ticketId)
+                );
+                if (thread) {
+                  await thread.setArchived(true);
+                }
+              }
+              if (replace == this.e.off || replace == this.e.idle) {
+                const ticketId = args[0];
+                const thread = await channel.threads?.cache.find((th) =>
+                  th.name.includes(ticketId)
+                );
+                if (thread) {
+                  await thread.setArchived(false);
+                }
+              }
+              msg.edit(this.replace(msg, replace));
+              return;
+            }
+          }
+        });
+      })
+      .then(callback(null))
+      .catch((err) => {
+        if (err) callback(err);
+      });
   }
-
 
   replace(msg, replace) {
     // return msg.content.split(this.e.off).join("").split(this.e.idle).join("").split(this.e.on).join(replace);
@@ -197,7 +224,6 @@ class Command extends Message.Event {
       return content.split(this.e.idle).join(replace);
     }
   }
-
 }
 
 const instance = new Command();
@@ -210,13 +236,13 @@ module.exports = {
   ignore: false,
   guildOnly: false,
   aliases: [],
-  permissions: ['DEV'],
+  permissions: ["DEV"],
   cooldown: 3,
-  color: 'RANDOM',
+  color: "RANDOM",
   extraFields: [],
   help: CommandName,
   call: async (message, client) => {
     if (!instance.initiated) instance.init(client);
     instance.call(message);
-  }
-}
+  },
+};
