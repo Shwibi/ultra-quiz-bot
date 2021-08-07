2640b879538ee61f952019e2f94c0f22f81b1e41
shwibi <shwibishrabs285@gmail.com>
2021-08-07 12:54:38 +0530

    LBD and Qban

--- commands/mod/qban.js
+++ commands/mod/qban.js
@@ -7,10 +7,9 @@ const Message = require(`../../events/message`);
 const { Cache, Err, Main } = require(`../../utils/Utils`);
 const Guilds = require("../../models/Guilds");
 
-const CommandName = 'QBan';
+const CommandName = "QBan";
 
 class Command extends Message.Event {
-
   /**
    * Create command
    */
@@ -19,9 +18,9 @@ class Command extends Message.Event {
   }
 
   /**
- * 
- * @param {Discord.Client} client 
- */
+   *
+   * @param {Discord.Client} client
+   */
   init(client) {
     if (this.initiated) return;
     this.client = client;
@@ -30,45 +29,57 @@ class Command extends Message.Event {
 
   /**
    * Call this command using message
-   * @param {Discord.Message} message 
+   * @param {Discord.Message} message
    */
   async call(message) {
-
-    if (!this.initiated) return new Err(`Called ${CommandName} command tendril without message initiation!`);
+    if (!this.initiated)
+      return new Err(
+        `Called ${CommandName} command tendril without message initiation!`
+      );
 
     const args = message.content.toLowerCase().split(/\s/);
     const rawCommand = args.shift();
 
-    if (args.length == 0) return message.channel.send(`Please provide the ID of the user or mention the user you want to ban!`);
+    if (args.length == 0)
+      return message.channel.send(
+        `Please provide the ID of the user or mention the user you want to ban!`
+      );
 
     const guildDB = await Guilds.findOne({ guildId: message.guild.id });
     if (!guildDB) return message.channel.send(`There was an error!`);
-    const userId = message.mentions.members.size !== 0 ? message.mentions.members.first().id : !isNaN(args[0]) ? args[0] : "invalid";
+    const userId =
+      message.mentions.members.size !== 0
+        ? message.mentions.members.first().id
+        : !isNaN(args[0])
+        ? args[0]
+        : "invalid";
     if (userId == "invalid") return message.channel.send(`Invalid user ID!`);
     const previousBanList = await guildDB.get("bannedUsers");
     if (args[1] == "unban") {
-      if (!previousBanList.includes(userId)) return message.channel.send(`User does not exist in ban list!`);
-      await guildDB.updateOne({
-        $pull: {
-          bannedUsers: userId
-        }
-      }).then(() => {
-        message.channel.send(`✅ Removed <@${userId}> from ban list!`);
-      })
+      if (!previousBanList.includes(userId))
+        return message.channel.send(`User does not exist in ban list!`);
+      await guildDB
+        .updateOne({
+          $pull: {
+            bannedUsers: userId,
+          },
+        })
+        .then(() => {
+          message.channel.send(`✅ Removed <@${userId}> from ban list!`);
+        });
+    } else {
+      if (previousBanList.includes(userId))
+        return message.channel.send(`User already exists in ban list!`);
+      await guildDB
+        .updateOne({
+          $push: {
+            bannedUsers: userId,
+          },
+        })
+        .then(() => {
+          message.channel.send(`✅ Added <@${userId}> to ban list!`);
+        });
     }
-    else {
-      if (previousBanList.includes(userId)) return message.channel.send(`User already exists in ban list!`);
-      await guildDB.updateOne({
-        $push: {
-          bannedUsers: userId
-        }
-      }).then(() => {
-        message.channel.send(`✅ Added <@${userId}> to ban list!`);
-      })
-    }
-
-
-
   }
 }
 
@@ -82,13 +93,13 @@ module.exports = {
   ignore: false,
   guildOnly: true,
   aliases: ["qb"],
-  permissions: ['SEND_MESSAGES', 'KICK_MEMBERS'],
-  cooldown: 3,
-  color: 'YELLOW',
+  permissions: ["SEND_MESSAGES", "KICK_MEMBERS"],
+  cooldown: 5,
+  color: "YELLOW",
   extraFields: [],
-  help: "`<prefix>qban <user ping/user id> [ban/unban]",
+  help: "`<prefix>qban <user ping/user id> [ban/unban]`",
   call: async (message, client) => {
     if (!instance.initiated) instance.init(client);
     instance.call(message);
-  }
-}
+  },
+};
