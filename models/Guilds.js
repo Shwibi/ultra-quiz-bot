1f993f9e03fdde3ad6a220843605159dceb2f2e2
shwibi <shwibishrabs285@gmail.com>
2021-08-07 10:58:47 +0530

    LBD Fetch

--- models/Guilds.js
+++ models/Guilds.js
@@ -1,14 +1,15 @@
 const { entryInstance } = require(`../index`);
 const config = require(`../store/config.json`);
 const reqString = { type: String, required: true };
-const unreqString = { type: String, required: false }
+const unreqString = { type: String, required: false };
 const GuildsSchema = new entryInstance.mongoose.Schema({
-    guildId: reqString,
-    guildName: reqString,
-    prefix: { type: String, required: true, default: config.Bot.prefix },
-    bannedUsers: { type: Array, required: true, default: [] },
-    leaderboard: { type: Array, required: false },
-    completed: { type: Array, required: false },
-    guildBoardChannel: unreqString
-})
-module.exports = entryInstance.mongo.model('Guilds', GuildsSchema);
+  guildId: reqString,
+  guildName: reqString,
+  prefix: { type: String, required: true, default: config.Bot.prefix },
+  bannedUsers: { type: Array, required: true, default: [] },
+  leaderboard: { type: Array, required: false },
+  completed: { type: Array, required: false },
+  guildBoardChannel: unreqString,
+  cache: { type: Array, required: false },
+});
+module.exports = entryInstance.mongo.model("Guilds", GuildsSchema);
--- utils/Main.js
+++ utils/Main.js
@@ -1,18 +1,17 @@
 const colors = require(`colors`);
 class Main {
-	constructor(name) {
-		name = name || "Bot";
-		name = `Bot.` + name;
-		this.name = name;
-		this.cache = [];
-    
+  constructor(name) {
+    name = name || "Bot";
+    name = `Bot.` + name;
+    this.name = name;
+    this.cache = [];
+
     this.UnknownError = `❌ An unknown error occured! Please try again!`;
+  }
 
-	}
-
-	InLog(...message) {
-		console.log(`[${this.name}]:`.yellow, ...message);
-	}
+  InLog(...message) {
+    console.log(`[${this.name}]:`.yellow, ...message);
+  }
 }
 
 module.exports = Main;
