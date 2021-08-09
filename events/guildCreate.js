62b307fcb3ef541ce94739c5fe2b6b4e4a2c6174
shwibi <shwibishrabs285@gmail.com>
2021-08-09 10:46:31 +0530

    Fix guildCreate event

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,62 +6,59 @@ const mongoose = require(`mongoose`);
 const { Cache, Err, Main } = require(`../utils/Utils`);
 const Guilds = require(`../models/Guilds`);
 
-const EventName = 'guildCreate';
+const EventName = "guildCreate";
 
 class Event extends Index.EntryPoint {
-    /**
-     * Create event
-     */
-    constructor() {
-        super(`event.${EventName}`);
-        this.initiated = false;
-    }
+	/**
+	 * Create event
+	 */
+	constructor() {
+		super(`event.${EventName}`);
+		this.initiated = false;
+	}
 
-    /**
-     * Initiate this event with client
-     * @param {Discord.Client} client 
-     */
-    init(client) {
-        if (this.initiated) return;
-        this.client = client;
-        this.initiated = true;
-    }
+	/**
+	 * Initiate this event with client
+	 * @param {Discord.Client} client
+	 */
+	init(client) {
+		if (this.initiated) return;
+		this.client = client;
+		this.initiated = true;
+	}
 
-    /**
-     * Call the event 
-     * @param {Discord.Guild} guild
-     */
-    async call(guild) {
+	/**
+	 * Call the event
+	 * @param {Discord.Guild} guild
+	 */
+	async call(guild) {
+		if (!this.initiated)
+			return new Err(`Called ${EventName} event without initialisation!`);
 
-        if (!this.initiated) return new Err(`Called ${EventName} event without initialisation!`);
+		// Fetch guild if exist
+		const existingGuild = await Guilds.findOne({ guildId: guild.id });
 
-        // Fetch guild if exist
-        const existingGuild = await Guilds.findOne({ guildId: guild.id });
-
-        if (!existingGuild) {
-            // If guild doesn't exist in database, add it
-            const guildDBEntry = await Guilds.create({
-                guildId: guild.id,
-                guildName: guild.name
-            })
-        }
-
-    }
+		if (!existingGuild) {
+			// If guild doesn't exist in database, add it
+			const guildDBEntry = await Guilds.create({
+				guildId: guild.id,
+				guildName: guild.name,
+			});
+		}
+	}
 }
 
 const Instance = new Event();
 
 // Exports
 module.exports = {
-    name: EventName,
-    call: (client) => {
+	name: EventName,
+	call: (client, guild) => {
+		// Call event
+		if (!Instance.initiated) Instance.init(client);
 
-        // Call event
-        if (!Instance.initiated) Instance.init(client);
-
-        Instance.call(guild);
-
-    },
-    Event,
-    Instance
-}
+		Instance.call(guild);
+	},
+	Event,
+	Instance,
+};
