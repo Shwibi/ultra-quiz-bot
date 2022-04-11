b6c571f439c8cf51201ccc72bfce320b3cac4997
shwibi <shwibishrabs285@gmail.com>
2022-04-11 14:56:06 +0530

    [NEW] Themes

--- utils/Main.js
+++ utils/Main.js
@@ -1,15 +1,26 @@
 const colors = require(`colors`);
 class Main {
-	constructor(name) {
-		name = name || "Bot";
-		name = `Bot.` + name;
-		this.name = name;
-		this.cache = [];
-	}
+  constructor(name) {
+    name = name || "Bot";
+    name = `Bot.` + name;
+    this.name = name;
+    this.cache = [];
+    this.config = require("../store/config.json");
+    const prs = [];
+    const ads = this.config.Ad.servers;
+    for (const key in ads) {
+      prs.push(`[**key**](${ads[key]})`);
+    }
+    this.prs = prs;
+  }
 
-	InLog(...message) {
-		console.log(`[${this.name}]:`.yellow, ...message);
-	}
+  get theme() {
+    return this.config.Bot.themes.winter;
+  }
+
+  InLog(...message) {
+    console.log(`[${this.name}]:`.yellow, ...message);
+  }
 }
 
 module.exports = Main;
